f96eaba82306d66b5e1742734e4f93178d76c41d
Nick Marucci <npm1514@gmail.com>
2018-12-29 15:11:48 -0700

    nodify and create cron

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const compression = require('compression');
+const fs = require('fs');
+const http = require('http');
+var CronJob = require('cron').CronJob;
+
+var port = process.env.PORT || 3000;
+var url = process.env.PORT ? "https://mosaic-fun.herokuapp.com/" : "http://localhost:" + port;
+var app = express();
+
+app.use(compression());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/health', (req, res) => {
+  console.log("Healthy");
+  res.send('Healthy')
+});
+
+app.listen(port, function(){
+  console.log("listening to " + port);
+});
+
+http.get(url + '/health');
+new CronJob('0 */15 * * * *', () => {
+  http.request(url + '/health');
+})
